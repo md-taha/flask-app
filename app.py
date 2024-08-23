7c2206a9c69d8b37266b9051e175bb0c202c3da5
Taha <mohammadtaha9935@gmail.com>
2024-08-23 19:31:32 +0530

    Updated

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_azure():
+@app.route("/")
+def index():
     return "Hello, Azure Web Apps!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
